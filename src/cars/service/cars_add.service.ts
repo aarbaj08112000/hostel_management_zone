9233f262384d797eb1992ebc8fa07ae0f0e0b4e4
nitesh.sharma <nitesh.sharma@hiddenbrains.in>
2025-07-07 12:11:12 +0530

    changes

--- src/cars/service/cars_add.service.ts
+++ src/cars/service/cars_add.service.ts
@@ -406,7 +406,9 @@ export class CarsAddService extends BaseService {
       if ('reserved_amount' in inputParams) {
         queryColumns.reservedAmount = inputParams.reserved_amount
       }
-      let generatedViews = await this.general.getRandomInt(3000,5000)
+      let viewGenerate = await this.general.getConfigItem('VIEWS_GENERATE');
+      viewGenerate = viewGenerate.split('-')
+      let generatedViews = await this.general.getRandomInt(viewGenerate[0],viewGenerate[1])
       queryColumns.generatedView = generatedViews;
       let code = await this.general.getCustomToken('cars', '', 'Add');
       if (code != '') {
